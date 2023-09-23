e26882f01ec669ac24b25cce34e5fba0e35a6f10
Alex Stepanov <stepanov.hw@gmail.com>
2023-09-22 21:52:26 -0600

    Add Show Delete and Update functionality in ChildCares

--- backend/controllers/childcares.js
+++ backend/controllers/childcares.js
@@ -2,7 +2,7 @@
 // DEPENDENCIES
 ////////////////////////////////
 
-const express = require('express')
+const express = require("express");
 const { sequelize, ChildCare } = require("../models");
 
 //     await sequelize.sync({force: true})
@@ -12,18 +12,18 @@ const { sequelize, ChildCare } = require("../models");
 ////////////////////////////////
 
 // CHILDCARES INDEX ACTION
-async function index(req,res,next) {
-	try {
+async function index(req, res, next) {
+  try {
     // get all childcares
     res.json(await ChildCare.findAll());
   } catch (error) {
     //send error
     res.status(400).json(error);
   }
-};
+}
 
 // CHILDCARES CREATE ACTION
-async function create(req,res,next) {
+async function create(req, res, next) {
   try {
     // create new childcare
     res.json(await ChildCare.create(req.body));
@@ -31,10 +31,50 @@ async function create(req,res,next) {
     //send error
     res.status(400).json(error);
   }
-};
+}
+
+// CHILDCARES SHOW ACTION
+async function show(req, res, next) {
+  try {
+    // send one childcare
+    res.status(200).json(await ChildCare.findByPk(req.params.id));
+  } catch (error) {
+    //send error
+    res.status(400).json({ error: error.message });
+  }
+}
+
+// CHILDCARES DESTROY ACTION
+async function destroy(req, res) {
+  try {
+    res.status(204).json(
+      await ChildCare.destroy({
+        where: { id: req.params.id },
+      })
+    );
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+}
+
+// CHILDCARES UPDATE ACTION
+async function update(req, res) {
+  try {
+    res.status(204).json(
+      await ChildCare.update(req.body, {
+        where: { id: req.params.id },
+      })
+    );
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+}
 
 // EXPORT Controller Action
 module.exports = {
-	index,
-	create,
-}
+  index,
+  create,
+  show,
+  delete: destroy,
+  update,
+};
--- backend/routes/childcares.js
+++ backend/routes/childcares.js
@@ -17,5 +17,14 @@ router.get("/", childCaresCtrl.index);
 // CHILDCARES CREATE ROUTE
 router.post("/", childCaresCtrl.create);
 
+// CHILDCARES SHOW ROUTE
+router.get("/:id", childCaresCtrl.show);
+
+// CHILDCARES DELETE ROUTE
+router.delete("/:id", childCaresCtrl.delete);
+
+// CHILDCARES UPDATE ROUTE
+router.put("/:id", childCaresCtrl.update);
+
 module.exports = router
 

c125d65de249df1da7f2cc52744247dff9c2baa9
Alex Stepanov <stepanov.hw@gmail.com>
2023-09-28 00:10:35 -0600

    Format text of the Card component

--- frontend/app/components/Card/index.js
+++ frontend/app/components/Card/index.js
@@ -2,15 +2,7 @@ import React from "react";
 import { UserIcon } from "@heroicons/react/24/outline";
 
 const Card = ({ childCare }) => {
-  const capitalizeEachWord = (str) => {
-    return str
-      .split(" ") // Split the string into an array of words
-      .map((word) => {
-        // Capitalize the first letter and convert the rest to lowercase for each word
-        return word.charAt(0).toUpperCase() + word.slice(1).toLowerCase();
-      })
-      .join(" "); // Join the words back into a single string with spaces
-  };
+  
   return (
     <div className="px-5 py-2">
       <div className="card lg:card-side bg-base-100 shadow-xl">
@@ -18,34 +10,23 @@ const Card = ({ childCare }) => {
           <img src="https://i.imgur.com/6qF1Hn6.jpg" alt="Album" />
         </figure>
         <div className="card-body lg:w-1/2 gap-0 p-1">
-          <p className="text-lg font-bold">
-            {capitalizeEachWord(childCare.name.toLowerCase())}
-          </p>
-          <p className="text-xs flex items-center">
-            {childCare.type[0] +
-              childCare.type.toLowerCase().slice(1)}
-          </p>
+          <p className="text-lg font-bold">{childCare.name}</p>
+          <p className="text-xs flex items-center">{childCare.type}</p>
           <p className="flex items-center text-sm">
             <UserIcon className="w-4 h-4" />
             {childCare.capacity}
           </p>
-          <p className="text-sm">
-            {capitalizeEachWord(childCare.address.toLowerCase())}
-          </p>
-          <p className="text-sm">{`${capitalizeEachWord(
-            childCare.city.toLowerCase()
-          )}, ${childCare.province}`}</p>
+          <p className="text-sm">{childCare.address}</p>
+          <p className="text-sm">{`${childCare.city}, ${childCare.province}`}</p>
           <p className="text-sm">{childCare.phoneNumber}</p>
-          {!childCare.website ? (
-            // <p className="text-sm">
-              <a
-                className="text-blue-600 visited:text-purple-600 text-sm"
-                href="https://example.com"
-                target="_blank"
-              >
-                example.com
-              </a>
-            // </p>
+          {childCare.website ? (
+            <a
+              className="text-blue-600 visited:text-purple-600 text-sm"
+              href={childCare.website}
+              target="_blank"
+            >
+              {shortenURL(childCare.website, 20)}
+            </a>
           ) : (
             ""
           )}
@@ -56,3 +37,26 @@ const Card = ({ childCare }) => {
 };
 
 export default Card;
+
+function shortenURL(originalURL) {
+  // Remove "http://", "https://", and "www." from the beginning of the URL
+  let cleanedURL = originalURL.replace(/^(https?:\/\/)?(www\.)?/, "");
+
+  // Find the index of the first slash ('/') to determine the domain
+  const slashIndex = cleanedURL.indexOf("/");
+  if (slashIndex !== -1) {
+    // Remove everything after the first slash to keep only the domain
+    cleanedURL = cleanedURL.substring(0, slashIndex);
+  }
+
+  return truncatedURL(cleanedURL, 30);
+}
+
+function truncatedURL(originalURL, maxLength) {
+  if (originalURL.length <= maxLength) {
+    return originalURL;
+  } else {
+    const truncatedURL = originalURL.substring(0, maxLength);
+    return truncatedURL + "...";
+  }
+}

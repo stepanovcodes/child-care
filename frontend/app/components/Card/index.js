a485ba05a29bbe35a0a226958302a918322e7ea9
Alex Stepanov <stepanov.hw@gmail.com>
2023-09-29 13:22:16 -0600

    Add daisyui to dependencies

--- frontend/app/components/Card/index.js
+++ frontend/app/components/Card/index.js
@@ -27,8 +27,8 @@ const Card = ({ childCare }) => {
       </p>
       <div className="card lg:card-side bg-base-100 shadow-xl">
         <figure className="lg:w-1/2">
-          <Image src="/assets/Child_Care_ERD.png" alt="Album" width="200" height="100"/>
-          {/* <Image src={`data:image/jpeg;base64,${base64Data}`} alt="Album" /> */}
+          <img src="https://i.imgur.com/6qF1Hn6.jpg" alt="Album"/>
+          {/* <img src={`data:image/jpeg;base64,${base64Data}`} alt="Album" /> */}
         </figure>
         <div className="flex-col lg:w-1/2 p-1">
           {childCare.rating !== null ? (

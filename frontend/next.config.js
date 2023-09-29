2ec1e57ef3743f88b758b60752db5a9f7ccd2025
Alex Stepanov <stepanov.hw@gmail.com>
2023-09-29 12:59:07 -0600

    Add NEXT_PUBLIC_MAPBOX_TOKEN to next.config.js

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -5,6 +5,7 @@ module.exports = {
     env: {
       NEXT_PUBLIC_MAPBOX_GL_ACCESS_TOKEN: process.env.NEXT_PUBLIC_MAPBOX_GL_ACCESS_TOKEN,
       NEXT_APP_BASE_URL: process.env.NEXT_APP_BASE_URL,
+      NEXT_PUBLIC_MAPBOX_TOKEN: process.env.NEXT_PUBLIC_MAPBOX_TOKEN,
     },
   };
 

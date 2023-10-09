7a4bf9101a21046a6e024d3b8fce80a65114b869
Alex Stepanov <stepanov.hw@gmail.com>
2023-10-09 10:37:03 -0600

    Delete dialog from Card component

--- frontend/app/components/Card/index.jsx
+++ frontend/app/components/Card/index.jsx
@@ -27,7 +27,6 @@ const Card = ({
     <div
       onMouseOver={handleMouseEnter}
       onMouseLeave={handleMouseLeave}
-      onClick={() => document.getElementById("my_modal_1").showModal()}
     >
       <Paper
         className="h-48 mx-5"
@@ -83,20 +82,6 @@ const Card = ({
           )}
         </div>
       </Paper>
-      <dialog id="my_modal_1" className="modal">
-        <div className="modal-box">
-          <h3 className="font-bold text-lg">Hello!</h3>
-          <p className="py-4">
-            Press ESC key or click the button below to close
-          </p>
-          <div className="modal-action">
-            <form method="dialog">
-              {/* if there is a button in form, it will close the modal */}
-              <button className="btn">Close</button>
-            </form>
-          </div>
-        </div>
-      </dialog>
     </div>
   );
 };

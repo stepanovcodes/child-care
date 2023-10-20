86c5c06c99dc371d46ce5ad240add7b1bb53db09
Alex Stepanov <stepanov.hw@gmail.com>
2023-10-20 12:09:58 -0600

    Add Google Analytics

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { getChildCares } from "@/app/utilities/childcares-service";
 import ChildCaresWrapper from '@/app/components/ChildCaresWrapper';
+import Script from 'next/script'
 
 interface ChildCare {
   uuid: string,
@@ -40,7 +41,21 @@ const HomePage = async () => {
     ...childCare,
     type: typeReplacements[childCare.type] || childCare.type,
   }));
-  return <ChildCaresWrapper childCares={childCares} />
+  return (
+    <>
+      <Script src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`} />
+      <Script id="google-analytics">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+ 
+          gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}');
+        `}
+      </Script>
+      <ChildCaresWrapper childCares={childCares} />
+    </>
+  );
 
 }
 
